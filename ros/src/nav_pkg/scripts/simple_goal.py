a43103e2d26264cb27686febd7f09748d2a88af5
王乐 <21371401@buaa.edu.cn>
2024-04-06 23:41:22 +0800

    fix(ros/navigation): 修复了导航模块下动态避障的Bug

--- ros/src/nav_pkg/scripts/simple_goal.py
+++ ros/src/nav_pkg/scripts/simple_goal.py
@@ -8,12 +8,13 @@ from std_msgs.msg import String
 import time  
 
 # 设置速度阈值  
-LINEAR_THRESHOLD = 0.01  # 例如，0.1 m/s  
-ANGULAR_THRESHOLD = 0.01  
+LINEAR_THRESHOLD = 0.00001  # 例如，0.1 m/s  
+ANGULAR_THRESHOLD = 0.00001  
 # 低速异常计数器
 slow_cnt = 0   
 goal = None
 ac = None
+reach_flag = False
 
 def done_cb(state, result):  
     if state == actionlib.GoalStatus.SUCCEEDED:  
@@ -27,11 +28,11 @@ def active_cb():
 
 
 def callback(msg): 
-    global slow_cnt,goal,ac
+    global slow_cnt,goal,ac,reach_flag
     linear = pow(msg.linear.x,2) + pow(msg.linear.y,2)  
     angular = abs(msg.angular.z)
     # 如果速度多次低于阈值，则重新设置航点  
-    if slow_cnt == 10:
+    if slow_cnt == 2 and reach_flag == False:
         slow_cnt = 0
         rospy.logwarn("机器人速度过慢，应该是遇到障碍，正在重新设置航点，新路径计算中...")  
         ac.cancel_goal()  # 取消当前目标  
@@ -41,14 +42,13 @@ def callback(msg):
         
         # 你可能需要实现一些逻辑来确保不会频繁地重置目标 
     
-    if linear  < LINEAR_THRESHOLD and angular < ANGULAR_THRESHOLD:  
+    if linear  < LINEAR_THRESHOLD and angular < ANGULAR_THRESHOLD and reach_flag == False:  
         slow_cnt += 1
-        time.sleep(1)
     else :
         slow_cnt = 0
 
 def start_nav(msg):
-    global ac,goal
+    global ac,goal,reach_flag
     ac = actionlib.SimpleActionClient('move_base', MoveBaseAction)  
     ac.wait_for_server()  
     # 处理String 类型消息
@@ -64,7 +64,9 @@ def start_nav(msg):
     # 在这里调用函数来监控速度并可能重新设置航点  
     vel_sub = rospy.Subscriber('/cmd_vel',Twist, callback)  
 
-    ac.wait_for_result()  
+    ac.wait_for_result()
+    reach_flag = True
+    slow_cnt = 0
       
     rospy.loginfo("导航结束！")
     result_msg = String()
