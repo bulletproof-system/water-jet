20aaf979342204f2931f43bf0f49a295565d800f
王乐 <lewang0454@foxmail.com>
2024-05-04 10:02:18 +0800

    fix(jet):修复了单模块测试中client端的bug

--- ros/src/robot_arm/src/client.cpp
+++ ros/src/robot_arm/src/client.cpp
@@ -4,12 +4,11 @@
 
 typedef actionlib::SimpleActionClient<robot_arm::AimAction> Client;
 
-// 当目标完成时，会调用这个回调函数
 void doneCb(const actionlib::SimpleClientGoalState& state,
             const robot_arm::AimResultConstPtr& result)
 {
   ROS_INFO("Finished in state [%s]", state.toString().c_str());
-  ROS_INFO("Result: %d", result->success); // 输出结果
+  ROS_INFO("Result: %d", result->success);
   ros::shutdown();
 }
 
@@ -17,15 +16,13 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "client");
   ros::NodeHandle nh;
-  Client client("aim", true); // 定义客户端，并指向服务器的名称 "aim"
+  Client client("/aim", true);
 
-  // 等待服务器启动
   ROS_INFO("Waiting for action server to start.");
   client.waitForServer();
 
-  // 创建并发送目标
   robot_arm::AimGoal goal;
-  goal.id = 1; // 假设的 pot_id 你可以根据需要修改这个值
+  goal.id = 1;
   client.sendGoal(goal, &doneCb);
 
   ros::spin();
