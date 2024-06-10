1988e1324b8d95b137c8579455029d1de965f50b
罗皓天 <21371229@buaa.edu.cn>
2024-06-10 11:18:13 +0800

    fix(frontend): 关闭 debug

--- frontend/src/stores/app.ts
+++ frontend/src/stores/app.ts
@@ -10,7 +10,7 @@ export enum ROSState {
 
 export const useAppStore = defineStore('app', {
   state: () => ({
-    debug: true,
+    debug: false,
     showPotList: true,
     showOperatorPanel: true,
     selectedPot: '',
