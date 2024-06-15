b82c8d8ea75f48fc22eea4c02065a7939bbcc65c
王乐 <lewang0454@foxmail.com>
2024-06-15 10:00:15 +0800

    fix(ros):修改了自动巡检和浇花机械臂模块

--- ros/src/robot_arm/src/arm.cpp
+++ ros/src/robot_arm/src/arm.cpp
@@ -174,14 +174,14 @@ private:
     vel_pub_.publish(vel_cmd);
 
     if (target_x <= 0.8) {
-      vel_cmd.linear.x = -0.6;
+      vel_cmd.linear.x = -0.4;
       vel_pub_.publish(vel_cmd);
       ros::Duration(1.0).sleep();
       vel_cmd.linear.x = 0;
       vel_pub_.publish(vel_cmd);      
     }
     else if (target_x > 1.5) {
-      vel_cmd.linear.x = 0.1;
+      vel_cmd.linear.x = 0.3;
       vel_pub_.publish(vel_cmd);
       ros::Duration(1.0).sleep();
       vel_cmd.linear.x = 0;
