376af0d9a3f5d294fe497f32f9d4bd127b9ef468
Mxode <max_zhang2020@outlook.com>
2024-06-17 20:43:20 +0800

    Update: 规整代码格式

--- ros/src/map_provider/scripts/assigner.py
+++ ros/src/map_provider/scripts/assigner.py
@@ -1,108 +1,103 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-#--------Include modules---------------
-from copy import copy
+#--------导入模块---------------
 import rospy
-from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Point
-from nav_msgs.msg import OccupancyGrid
-import tf
-from map_provider.msg import PointArray
+from copy import copy
 from numpy import array
-from numpy import linalg as LA
-from numpy import all as All
-from numpy import inf
-from functions import Robot, informationGain, discount
 from numpy.linalg import norm
 from collections import defaultdict
+from nav_msgs.msg import OccupancyGrid
+from map_provider.msg import PointArray
+from functions import Robot, informationGain, discount
 
-# Subscribers' callbacks------------------------------
-mapData=OccupancyGrid()
-frontiers=[]
-global1=OccupancyGrid()
-global2=OccupancyGrid()
-global3=OccupancyGrid()
-globalmaps=[]
+# 订阅者的回调函数------------------------------
+mapData = OccupancyGrid()
+frontiers = []
+global1 = OccupancyGrid()
+global2 = OccupancyGrid()
+global3 = OccupancyGrid()
+globalmaps = []
+
 def callBack(data):
-	global frontiers
-	frontiers=[]
-	for point in data.points:
-		frontiers.append(array([point.x,point.y]))
+    global frontiers
+    frontiers = []
+    for point in data.points:
+        frontiers.append(array([point.x, point.y]))
 
 def mapCallBack(data):
     global mapData
-    mapData=data
+    mapData = data
+
 # Node----------------------------------------------
 
 def node():
-	global frontiers,mapData,global1,global2,global3,globalmaps
+	global frontiers, mapData, global1, global2, global3, globalmaps
 	rospy.init_node('assigner', anonymous=False)
 	
-	# fetching all parameters
-	map_topic= rospy.get_param('~map_topic','/map')
-	info_radius= rospy.get_param('~info_radius',1.0)					#this can be smaller than the laser scanner range, >> smaller >>less computation time>> too small is not good, info gain won't be accurate
-	info_multiplier=rospy.get_param('~info_multiplier',3.0)		
-	hysteresis_radius=rospy.get_param('~hysteresis_radius',3.0)			#at least as much as the laser scanner range
-	hysteresis_gain=rospy.get_param('~hysteresis_gain',2.0)				#bigger than 1 (biase robot to continue exploring current region
-	frontiers_topic= rospy.get_param('~frontiers_topic','/filtered_points')	
-	n_robots = rospy.get_param('~n_robots',1)
-	namespace = rospy.get_param('~namespace','')
+	# 获取所有参数	
+	map_topic = rospy.get_param('~map_topic', '/map')
+	info_radius = rospy.get_param('~info_radius', 1.0)  # 这个值可以小于激光扫描器的范围，越小计算时间越短，但太小信息增益不准确
+	info_multiplier = rospy.get_param('~info_multiplier', 3.0)
+	hysteresis_radius = rospy.get_param('~hysteresis_radius', 3.0)  # 至少与激光扫描器范围一样大
+	hysteresis_gain = rospy.get_param('~hysteresis_gain', 2.0)  # 大于1，偏向机器人继续探索当前区域
+	frontiers_topic = rospy.get_param('~frontiers_topic', '/filtered_points')
+	n_robots = rospy.get_param('~n_robots', 1)
+	namespace = rospy.get_param('~namespace', '')
 	namespace_init_count = rospy.get_param('namespace_init_count', 1)
-	delay_after_assignement=rospy.get_param('~delay_after_assignement', 0.3)
-	rateHz = rospy.get_param('~rate',100)
+	delay_after_assignement = rospy.get_param('~delay_after_assignement', 0.3)
+	rateHz = rospy.get_param('~rate', 100)
 	
 	rate = rospy.Rate(rateHz)
-#-------------------------------------------
+
 	rospy.Subscriber(map_topic, OccupancyGrid, mapCallBack)
 	rospy.Subscriber(frontiers_topic, PointArray, callBack)
-#---------------------------------------------------------------------------------------------------------------
-		
-# wait if no frontier is received yet 
-	while len(frontiers)<1:
+
+	# 等待接收前沿点
+	while len(frontiers) < 1:
 		pass
-	centroids=copy(frontiers)	
-#wait if map is not received yet
-	while (len(mapData.data)<1):
+	centroids = copy(frontiers)
+    
+    # 等待接收地图数据
+	while len(mapData.data) < 1:
 		pass
 
+	# 初始化机器人
 	robots = []
-	if len(namespace)>0:
-		for i in range(0,n_robots):
-			robots.append(Robot(namespace+str(i+namespace_init_count)))
-	elif len(namespace)==0:
-			robots.append(Robot(namespace))
-	for i in range(0,n_robots):
+	if len(namespace) > 0:
+		for i in range(n_robots):
+			robots.append(Robot(namespace + str(i + namespace_init_count)))
+	else:
+		robots.append(Robot(namespace))
+	for i in range(n_robots):
 		robots[i].sendGoal(robots[i].getPosition())
-#-------------------------------------------------------------------------
-#---------------------     Main   Loop     -------------------------------
-#-------------------------------------------------------------------------
-	# TTL dict & set
-	ttl_dict = defaultdict()	# 用于统计目标点的访问次数
-	ttl_set = set()				# 用于收纳 pop 掉的点
 
+	# TTL字典和集合
+	ttl_dict = defaultdict()  	# 用于统计目标点的访问次数
+	ttl_set = set()  			# 用于收纳被移除的点
+
+	# 主循环
 	while not rospy.is_shutdown():
 		centroids = copy(frontiers)		
-#-------------------------------------------------------------------------			
-#Get information gain for each frontier point
+
+		# 获取每个前沿点的信息增益
 		infoGain = []
-		for ip in range(0, len(centroids)):
-			infoGain.append(informationGain(mapData,[centroids[ip][0],centroids[ip][1]],info_radius))
-#-------------------------------------------------------------------------			
-#get number of available/busy robots
-		na = [] #available robots
-		nb = [] #busy robots
-		for i in range(0, n_robots):
-			if (robots[i].getState() == 1):
+		for ip in range(len(centroids)):
+			infoGain.append(informationGain(mapData, [centroids[ip][0], centroids[ip][1]], info_radius))
+
+		# 获取可用和忙碌的机器人数量
+		na = []  # 可用机器人
+		nb = []  # 忙碌机器人
+		for i in range(n_robots):
+			if robots[i].getState() == 1:
 				nb.append(i)
 			else:
-				na.append(i)	
-		# rospy.loginfo("available robots: " + str(na))
-#------------------------------------------------------------------------- 
-#get dicount and update informationGain
-		for i in nb+na:
-			infoGain=discount(mapData,robots[i].assigned_point,centroids,infoGain,info_radius)
-#-------------------------------------------------------------------------            
+				na.append(i)
+		
+		# 更新信息增益
+		for i in nb + na:
+			infoGain = discount(mapData, robots[i].assigned_point, centroids, infoGain, info_radius)
+		
 		revenue_record = []
 		centroid_record = []
 		id_record = []
@@ -121,7 +116,6 @@ def node():
 				
 				# 检查目标是否可达
 				if robots[ir].isReachable(robots[ir].getPosition(), centroids[ip]):
-					rospy.loginfo("all_unreachable -> False")
 					all_unreachable = False  # 有一个点可达，标识位设为 False
 					revenue_record.append(revenue)
 					centroid_record.append(centroids[ip])
@@ -150,8 +144,7 @@ def node():
 			id_record = []
 			for ir in nb:
 				for ip in range(0, len(centroids)):
-					cost = norm(robots[ir].getPosition() - centroids[ip])		
-					threshold = 1
+					cost = norm(robots[ir].getPosition() - centroids[ip])
 					information_gain=infoGain[ip]
 					if (norm(robots[ir].getPosition() - centroids[ip]) <= hysteresis_radius):
 						information_gain *= hysteresis_gain
@@ -189,7 +182,7 @@ def node():
 				ttl_cnt = ttl_dict.get(tuple_target_point, 0) + 1
 				ttl_dict[tuple_target_point] = ttl_cnt
 				if ttl_cnt > 5:
-					rospy.logwarn("point %s ttl -> 5, pop!", target_point)
+					rospy.logwarn("[map_provider - auto_map - assigner.py] point %s ttl -> 5, pop!", target_point)
 					ttl_set.add(tuple_target_point)
 					centroid_record.pop(winner_id)
 					revenue_record.pop(winner_id)
@@ -210,9 +203,8 @@ def node():
 				for r in robots:
 					r.sendGoal(array([0.0, 0.0]), exit=True)
 				break
-#------------------------------------------------------------------------- 
+		
 		rate.sleep()
-#-------------------------------------------------------------------------
 
 if __name__ == '__main__':
     try:
