bd7affe94663002309b8594d23da998ff93fb2c0
Mxode <max_zhang2020@outlook.com>
2024-06-21 02:27:25 +0800

    Update: 规整代码格式

--- ros/src/map_provider/scripts/frontier_opencv_detector.py
+++ ros/src/map_provider/scripts/frontier_opencv_detector.py
@@ -6,40 +6,42 @@ import rospy
 from visualization_msgs.msg import Marker
 from nav_msgs.msg import OccupancyGrid
 from geometry_msgs.msg import PointStamped
-from getfrontier import getfrontier
+from get_frontier import get_frontier
 
 # 订阅者的回调函数
-mapData = OccupancyGrid()
+map_data = OccupancyGrid()
 
-def mapCallBack(data):
-    global mapData
-    mapData = data
+
+def map_callback(data):
+    global map_data
+    map_data = data
+
 
 # 节点
 def node():
-    global mapData
+    global map_data
     exploration_goal = PointStamped()
     rospy.init_node('detector', anonymous=False)
-    
+
     # 获取参数
     map_topic = rospy.get_param('~map_topic', '/robot_1/map')
-    
+
     # 订阅地图话题
-    rospy.Subscriber(map_topic, OccupancyGrid, mapCallBack)
-    
+    rospy.Subscriber(map_topic, OccupancyGrid, map_callback)
+
     # 发布检测到的点和标记
-    targetspub = rospy.Publisher('/detected_points', PointStamped, queue_size=10)
+    targets_pub = rospy.Publisher('/detected_points', PointStamped, queue_size=10)
     pub = rospy.Publisher('shapes', Marker, queue_size=10)
-    
+
     # 等待接收地图数据
-    while mapData.header.seq < 1 or len(mapData.data) < 1:
+    while map_data.header.seq < 1 or len(map_data.data) < 1:
         pass
-    
+
     rate = rospy.Rate(50)
     points = Marker()
 
     # 设置标记的帧ID和时间戳
-    points.header.frame_id = mapData.header.frame_id
+    points.header.frame_id = map_data.header.frame_id
     points.header.stamp = rospy.Time.now()
 
     points.ns = "markers"
@@ -58,21 +60,22 @@ def node():
 
     # 使用 OpenCV 进行前沿检测
     while not rospy.is_shutdown():
-        frontiers = getfrontier(mapData)
+        frontiers = get_frontier(map_data)
         for i in range(len(frontiers)):
             x = frontiers[i]
-            exploration_goal.header.frame_id = mapData.header.frame_id
+            exploration_goal.header.frame_id = map_data.header.frame_id
             exploration_goal.header.stamp = rospy.Time(0)
             exploration_goal.point.x = x[0]
             exploration_goal.point.y = x[1]
             exploration_goal.point.z = 0
 
             # 发布检测到的点
-            targetspub.publish(exploration_goal)
+            targets_pub.publish(exploration_goal)
             points.points = [exploration_goal.point]
             pub.publish(points)
-        
+
         rate.sleep()
+
 
 if __name__ == '__main__':
     try:
