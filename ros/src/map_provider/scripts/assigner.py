c93cad34f26db432ba6fa102c9cb2fdacc0ae244
Mxode <max_zhang2020@outlook.com>
2024-06-21 02:27:18 +0800

    Update: 规整代码格式

--- ros/src/map_provider/scripts/assigner.py
+++ ros/src/map_provider/scripts/assigner.py
@@ -12,12 +12,13 @@ from map_provider.msg import PointArray
 from functions import Robot, informationGain, discount
 
 # 订阅者的回调函数
-mapData = OccupancyGrid()
+map_data = OccupancyGrid()
 frontiers = []
 global1 = OccupancyGrid()
 global2 = OccupancyGrid()
 global3 = OccupancyGrid()
-globalmaps = []
+global_maps = []
+
 
 def callBack(data):
     global frontiers
@@ -25,15 +26,17 @@ def callBack(data):
     for point in data.points:
         frontiers.append(array([point.x, point.y]))
 
+
 def mapCallBack(data):
-    global mapData
+    global map_data
     mapData = data
+
 
 # 节点
 def node():
-    global frontiers, mapData, global1, global2, global3, globalmaps
+    global frontiers, map_data, global1, global2, global3, globalmaps
     rospy.init_node('assigner', anonymous=False)
-    
+
     # 获取所有参数    
     map_topic = rospy.get_param('~map_topic', '/map')
     info_radius = rospy.get_param('~info_radius', 1.0)  # 这个值可以小于激光扫描器的范围，越小计算时间越短，但太小信息增益不准确
@@ -46,7 +49,7 @@ def node():
     namespace_init_count = rospy.get_param('namespace_init_count', 1)
     delay_after_assignement = rospy.get_param('~delay_after_assignement', 0.3)
     rateHz = rospy.get_param('~rate', 100)
-    
+
     rate = rospy.Rate(rateHz)
 
     rospy.Subscriber(map_topic, OccupancyGrid, mapCallBack)
@@ -56,7 +59,7 @@ def node():
     while len(frontiers) < 1:
         pass
     centroids = copy(frontiers)
-    
+
     # 等待接收地图数据
     while len(mapData.data) < 1:
         pass
@@ -69,15 +72,15 @@ def node():
     else:
         robots.append(Robot(namespace))
     for i in range(n_robots):
-        robots[i].sendGoal(robots[i].getPosition())
+        robots[i].send_nav_goal(robots[i].get_position())
 
     # TTL字典和集合
-    ttl_dict = defaultdict()      # 用于统计目标点的访问次数
-    ttl_set = set()              # 用于收纳被移除的点
+    ttl_dict = defaultdict()  # 用于统计目标点的访问次数
+    ttl_set = set()  # 用于收纳被移除的点
 
     # 主循环
     while not rospy.is_shutdown():
-        centroids = copy(frontiers)        
+        centroids = copy(frontiers)
 
         # 获取每个前沿点的信息增益
         infoGain = []
@@ -88,83 +91,87 @@ def node():
         na = []  # 可用机器人
         nb = []  # 忙碌机器人
         for i in range(n_robots):
-            if robots[i].getState() == 1:
+            if robots[i].get_state() == 1:
                 nb.append(i)
             else:
                 na.append(i)
-        
+
         # 更新信息增益
         for i in nb + na:
             infoGain = discount(mapData, robots[i].assigned_point, centroids, infoGain, info_radius)
-        
+
         revenue_record = []
         centroid_record = []
         id_record = []
-        
+
         escape_mode = False
         # 在 sendGoal 之前添加可达性检查
         for ir in na:
             all_unreachable = True  # 标识位，假设所有点都不可达
             for ip in range(0, len(centroids)):
-                cost = norm(robots[ir].getPosition() - centroids[ip])
+                cost = norm(robots[ir].get_position() - centroids[ip])
                 information_gain = infoGain[ip]
-                if (norm(robots[ir].getPosition() - centroids[ip]) <= hysteresis_radius):
+                if (norm(robots[ir].get_position() - centroids[ip]) <= hysteresis_radius):
                     information_gain *= hysteresis_gain
                 revenue = information_gain * info_multiplier - cost
-                
+
                 # 检查目标是否可达
-                if robots[ir].isReachable(robots[ir].getPosition(), centroids[ip]):
+                if robots[ir].is_reachable(robots[ir].get_position(), centroids[ip]):
                     all_unreachable = False  # 有一个点可达，标识位设为 False
                     revenue_record.append(revenue)
                     centroid_record.append(centroids[ip])
                     id_record.append(ir)
-            
+
             # 如果所有点都不可达，尝试脱困
             if all_unreachable:
                 rospy.logwarn("[map_provider - auto_map - assigner.py] all unreachable, try to escape")
                 escape_mode = True
-                nearby_point = robots[ir].getNearbyReachablePoint()
+                nearby_point = robots[ir].get_nearby_reachable_points()
                 if nearby_point is not None:
-                    rospy.loginfo("[map_provider - auto_map - assigner.py] Found nearby reachable point for robot %s: %s", ir, nearby_point)
+                    rospy.loginfo(
+                        "[map_provider - auto_map - assigner.py] Found nearby reachable point for robot %s: %s", ir,
+                        nearby_point)
                     revenue_record.append(0)  # 脱困点的收益设为0
                     centroid_record.append(nearby_point)
                     id_record.append(ir)
                     rospy.loginfo("nearby_point is not None")
-                    robots[ir].sendGoal(nearby_point, escape=True)
+                    robots[ir].send_nav_goal(nearby_point, escape=True)
                     rospy.sleep(delay_after_assignement)
                 else:
-                    rospy.logwarn("[map_provider - auto_map - assigner.py] No nearby reachable point found for robot %s.", ir)
+                    rospy.logwarn(
+                        "[map_provider - auto_map - assigner.py] No nearby reachable point found for robot %s.", ir)
                 break
-        
+
         if len(na) < 1:
             revenue_record = []
             centroid_record = []
             id_record = []
             for ir in nb:
                 for ip in range(0, len(centroids)):
-                    cost = norm(robots[ir].getPosition() - centroids[ip])
-                    information_gain=infoGain[ip]
-                    if (norm(robots[ir].getPosition() - centroids[ip]) <= hysteresis_radius):
+                    cost = norm(robots[ir].get_position() - centroids[ip])
+                    information_gain = infoGain[ip]
+                    if (norm(robots[ir].get_position() - centroids[ip]) <= hysteresis_radius):
                         information_gain *= hysteresis_gain
-                
-                    if ((norm(centroids[ip]-robots[ir].assigned_point))<hysteresis_radius):
-                        information_gain = informationGain(mapData, [centroids[ip][0], centroids[ip][1]], info_radius) * hysteresis_gain
+
+                    if ((norm(centroids[ip] - robots[ir].assigned_point)) < hysteresis_radius):
+                        information_gain = informationGain(mapData, [centroids[ip][0], centroids[ip][1]],
+                                                           info_radius) * hysteresis_gain
 
                     revenue = information_gain * info_multiplier - cost
                     revenue_record.append(revenue)
                     centroid_record.append(centroids[ip])
                     id_record.append(ir)
-        
+
         if len(revenue_record) < 1:
             # 取消所有目标
             rospy.logwarn("[map_provider - auto_map - assigner.py] No available revenue records, cancelling all goals.")
             for r in robots:
-                r.cancelGoal()
+                r.cancel_nav_goal()
             rospy.sleep(1)
             # 向 (0, 0) 发送导航目标
             rospy.loginfo("[map_provider - auto_map - assigner.py] Navigating all robots to (0, 0).")
             for r in robots:
-                r.sendGoal(array([0.0, 0.0]), exit=True)
+                r.send_nav_goal(array([0.0, 0.0]), exit=True)
             break
 
         target_flag = False
@@ -185,24 +192,26 @@ def node():
                     centroid_record.pop(winner_id)
                     revenue_record.pop(winner_id)
                 else:
-                    robots[id_record[winner_id]].sendGoal(target_point, escape=escape_mode)
+                    robots[id_record[winner_id]].send_nav_goal(target_point, escape=escape_mode)
                     rospy.sleep(delay_after_assignement)
                     escape_mode = False  # 重置标识位
                     target_flag = True
                     break
             if not target_flag:
                 # 取消所有目标
-                rospy.logwarn("[map_provider - auto_map - assigner.py] No available revenue records, cancelling all goals.")
+                rospy.logwarn(
+                    "[map_provider - auto_map - assigner.py] No available revenue records, cancelling all goals.")
                 for r in robots:
-                    r.cancelGoal()
+                    r.cancel_nav_goal()
                 rospy.sleep(1)
                 # 向 (0, 0) 发送导航目标
                 rospy.loginfo("[map_provider - auto_map - assigner.py] Navigating all robots to (0, 0).")
                 for r in robots:
-                    r.sendGoal(array([0.0, 0.0]), exit=True)
+                    r.send_nav_goal(array([0.0, 0.0]), exit=True)
                 break
-        
+
         rate.sleep()
+
 
 if __name__ == '__main__':
     try:
