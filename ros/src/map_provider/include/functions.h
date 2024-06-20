0fdd81fd4df1b21c44bb6ef99953231b306d8a93
Mxode <max_zhang2020@outlook.com>
2024-06-21 06:01:27 +0800

    Update: 规整代码格式

--- ros/src/map_provider/include/functions.h
+++ ros/src/map_provider/include/functions.h
@@ -1,5 +1,6 @@
-#ifndef functions_H
-#define functions_H
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
 #include "ros/ros.h"
 #include <vector>
 #include <stdlib.h>
@@ -9,30 +10,30 @@
 #include "geometry_msgs/Point.h"
 #include "visualization_msgs/Marker.h"
 
-// rdm class, for gentaring random flot numbers
-class rdm{
-int i;
+// rdm 类，用于生成随机浮点数
+class rdm {
+    int i;
 public:
-rdm();
-float randomize();
+    rdm();
+    float randomize();
 };
 
+// Norm 函数原型，计算两个点之间的欧几里得距离
+float Norm(std::vector<float>, std::vector<float>);
 
-//Norm function prototype
-float Norm( std::vector<float> , std::vector<float> );
+// sign 函数原型，返回数值的符号
+float sign(float);
 
-//sign function prototype
-float sign(float );
+// Nearest 函数原型，找到距离 x 最近的点
+std::vector<float> Nearest(std::vector<std::vector<float>>, std::vector<float>);
 
-//Nearest function prototype
-std::vector<float> Nearest(  std::vector< std::vector<float>  > , std::vector<float> );
+// Steer 函数原型，计算新的点
+std::vector<float> Steer(std::vector<float>, std::vector<float>, float);
 
-//Steer function prototype
-std::vector<float> Steer(  std::vector<float>, std::vector<float>, float );
+// gridValue 函数原型，返回 "Xp" 位置的网格值
+int gridValue(nav_msgs::OccupancyGrid&, std::vector<float>);
 
-//gridValue function prototype
-int gridValue(nav_msgs::OccupancyGrid &,std::vector<float>);
+// ObstacleFree 函数原型，检查路径是否无障碍
+char ObstacleFree(std::vector<float>, std::vector<float>&, nav_msgs::OccupancyGrid);
 
-//ObstacleFree function prototype
-char ObstacleFree(std::vector<float> , std::vector<float> & , nav_msgs::OccupancyGrid);
-#endif
+#endif // FUNCTIONS_H
